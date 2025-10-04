eb085e6d2bfd478f76765695b8c156c1a84fcb89
MD Jomman <75951818+RabiulHassanJomman@users.noreply.github.com>
2025-10-04 23:18:42 +0600

    team layout fixed

--- src/pages/Team.js
+++ src/pages/Team.js
@@ -63,7 +63,7 @@ const Team = () => {
           </p>
 
           {/* Team members grid - Responsive layout */}
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {teamMembers.map((member, index) => (
               <div
                 key={index}
